f490b5abb8447bd3bf677111eafef5904e208ac9
Rylkov Evgenii <Rylkow@yandex.ru>
2022-10-31 22:16:34 +0600

    add class Cli add greeting in Main

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,28 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    private String name;
+
+    Cli(String name){
+        this.name = name;
+    }
+
+    String getName(){
+        return name;
+    }
+
+    void setName(String name){
+        this.name = name;
+    }
+
+    static void firstGreeting(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("May I have your name?");
+        String userName = scanner.next();
+        scanner.close();
+        var user =  new Cli(userName);
+        System.out.println("Hello, " + user.getName() + "!");
+    }
+}
