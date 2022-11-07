2487b28e6ce63ddb84b5a73b0383a2d4789c1548
RylkovEvgenii <Rylkowe@yandex.ru>
2022-11-07 17:22:36 +0600

    fix bugs with zero in GCD

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -19,8 +19,11 @@ public class GCD {
         int number2 = Integer.parseInt(words[1]);
         int min = Math.min(number1, number2);
         int max = Math.max(number1, number2);
+        if (min == 0) {
+            return Integer.toString(max);
+        }
         int result = 1;
-        for (int i = min; i >= 0; i--) {
+        for (int i = min; i != 1; i--) {
             if (max % i == 0 & min % i == 0) {
                 result = i;
                 break;
